d587a5a4326f98260f3df2b36e4b22da84164340
choi <chj5156@naver.com>
2020-12-15 15:34:58 +0900

    프로젝트시작

---
+++ MatchFootball/src/main/java/co/company/spring/doeun/dd.java
@@ -0,0 +1,7 @@
+package co.company.spring.doeun;
+
+public class dd {
+public static void main(String[] args) {
+	System.out.println("1");
+}
+}
---
+++ MatchFootball/src/main/java/co/company/spring/sungjun/dd.java
@@ -0,0 +1,7 @@
+package co.company.spring.sungjun;
+
+public class dd {
+public static void main(String[] args) {
+	System.out.println("d");
+}
+}
