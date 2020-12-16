788c1cc33dee7a56777d9c361819c0122d26a5cb
kisssky12 <kisssky12@gamil.com>
2020-12-16 15:06:56 +0900

    ma

--- MatchFootball/src/main/java/co/company/spring/seemoo/AdminController.java
+++ MatchFootball/src/main/java/co/company/spring/seemoo/AdminController.java
@@ -1,21 +1,12 @@
 package co.company.spring.seemoo;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AdminController {
 
-	@RequestMapping(value="/admin")
-	public ModelAndView test(HttpServletResponse response) throws IOException{
-		return new ModelAndView("home");
-	}
-	@RequestMapping("/")
+	@RequestMapping("/admin")
 	public String main() {
 		return "seemoo/index";
 	}
