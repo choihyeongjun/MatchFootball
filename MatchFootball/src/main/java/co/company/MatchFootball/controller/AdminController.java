15d18e98d8128385e0b11b6c8e70bcd679f77717
choi <chj5156@naver.com>
2020-12-16 15:02:59 +0900

    ddddddddd

--- MatchFootball/src/main/java/co/company/MatchFootball/controller/AdminController.java
+++ MatchFootball/src/main/java/co/company/MatchFootball/controller/AdminController.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class AdminController {
 
+	
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
 		return new ModelAndView("home");
